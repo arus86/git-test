2514e14e5732646aff5fbcc483d63deef85ac8ab
LoADing <549082017@qq.com>
2024-07-12 16:28:04 +0800

    hello  git

---
+++ Test.java
@@ -0,0 +1,12 @@
+/**
+ * Class: Test
+ * Author: LoADing.
+ * Date: 2024/7/12
+ * Description:
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
